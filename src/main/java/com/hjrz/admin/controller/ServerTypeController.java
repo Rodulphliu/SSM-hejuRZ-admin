96485dbb9cdbaa4498fbd0fcf43c84b2fb4d0f0a
RodulphLiu <22903594@qq.com>
2017-05-13 17:24:46 +0800

    测试

--- src/main/java/com/hjrz/admin/controller/ServerTypeController.java
+++ src/main/java/com/hjrz/admin/controller/ServerTypeController.java
@@ -1,23 +1,12 @@
 package com.hjrz.admin.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.aspectj.apache.bcel.generic.ReturnaddressType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.hjrz.admin.constants.CallStatusEnum;
-import com.hjrz.admin.data.ExchangeData;
-import com.hjrz.admin.entity.ServerType;
-import com.hjrz.admin.model.ServerTypeModel;
 import com.hjrz.admin.service.ServerTypeService;
 
 /**
@@ -59,7 +48,7 @@ public class ServerTypeController {
        * @author RudolphLiu
        * @Date 2017年5月11日 上午10:51:16
        */
-      @SuppressWarnings("rawtypes")
+      /*@SuppressWarnings("rawtypes")
       @RequestMapping(value="/addservertype.do",method= RequestMethod.POST)
       public ExchangeData addServerType(ServerType serverType)
       {
@@ -70,5 +59,25 @@ public class ServerTypeController {
           exchangeData.markException(e);
         }
         return exchangeData;
+      }*/
+        
+      @RequestMapping(value="/gotoajax")
+      public String gotoaddAjax()
+      {
+        return "tohello";
       }
+      
+      
+      @RequestMapping(value="/adserver.do",method= RequestMethod.POST)
+      public ModelAndView addserverType(String username,String password)
+      {
+          ModelAndView modelAndView = new ModelAndView("");
+          try {
+            modelAndView.setViewName("");
+          } catch (Exception e) {
+            // TODO: handle exception
+          }
+          return modelAndView;
+      }
+      
 }
